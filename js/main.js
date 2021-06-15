cfb8b99c038eea1209ffbbf7cba09bf016d45270
AlexanderGubanov <ag123@mail.ru>
2021-06-15 11:53:46 +0300

    Внес исправления в код

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
-let randomInteger = function(min, max) {
-  let result = min - 0.5 + Math.random() * (max - min + 1);
+
+const randomInteger = function(min, max) {
   if (min >= 0 && min < max) {
-  return Math.round(result);
-  }
- console.log('Диапазон значений указан неправильно');
-  }
+    let result = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(result);
+    }
+    console.log('Диапазон значений указан неправильно');
+  };
 
 console.log(randomInteger(12, 99));
 
 // Источник формулы для расчета переменной result: https://learn.javascript.ru/task/random-int-min-max
 
 
-let randomFloatNumber = function(min, max, numberDecimals) {
-  let result = min - 0.5 + Math.random() * (max - min + 1);
-  result = parseFloat(result.toFixed(numberDecimals));
-  if (min >= 0 && min < max) {
-  return result;
-  }
- console.log('Диапазон значений указан неправильно');
-  }
+const randomFloatNumber = function(min, max, numberDecimals) {
+    if (min >= 0 && min < max) {
+      let result = Math.random() * (max - min) + min;
+      result = parseFloat(result.toFixed(numberDecimals));
+      return result;
+      }
+      console.log('Диапазон значений указан неправильно');
+      };
 
-console.log(randomFloatNumber(1, 500, 5));
+console.log(randomFloatNumber(1.11, 1.12, 4));
